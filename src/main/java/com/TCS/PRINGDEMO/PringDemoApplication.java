6e010f529295ee3eaaeab6aaa47657e2d6cc8a5a
Hellen Villanueva Astudillo - TCS <S62991@domain.primaafp.com.pe>
2018-08-25 16:11:53 -0500

    Primera versiom

---
+++ src/main/java/com/TCS/PRINGDEMO/PringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.TCS.PRINGDEMO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PringDemoApplication.class, args);
+	}
+}
